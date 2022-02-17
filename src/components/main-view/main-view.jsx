df2e930d4ea718f5ac27de8292196e60e4c08111
valvegan <valentinavaccari@hotmail.com>
2022-02-17 18:31:32 +0000

    axios - componentDidMount()

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import axios from 'axios';
+
+
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
@@ -6,19 +9,21 @@ export class MainView extends React.Component {
     constructor(){
         super();
         this.state = {
-          movies: [
-            { _id: 1, Title: 'Inception', 
-            Description: 'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O., but his tragic past may doom the project and his team to disaster.', 
-            ImagePath: 'https://m.media-amazon.com/images/M/MV5BMTM0MjUzNjkwMl5BMl5BanBnXkFtZTcwNjY0OTk1Mw@@._V1_.jpg'},
-            { _id: 2, Title: 'The Shawshank Redemption', 
-            Description: 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.', 
-            ImagePath: 'https://flxt.tmsimg.com/assets/p15987_p_v8_ai.jpg'},
-            { _id: 3, Title: 'Gladiator', 
-            Description: 'A former Roman General sets out to exact vengeance against the corrupt emperor who murdered his family and sent him into slavery.', 
-            ImagePath: 'https://flxt.tmsimg.com/assets/p24674_p_v8_ae.jpg'}
-          ],
+          movies: [],
           selectedMovie: null
         }
+      }
+
+      componentDidMount(){
+        axios.get('https://my-flix-api-2022.herokuapp.com/movies')
+        .then(response=>{
+          this.setState({
+            movies: response.data
+          });
+        })
+        .catch(error=>{
+          console.log(error);
+        })
       }
 
       setSelectedMovie (newSelectedMovie){
