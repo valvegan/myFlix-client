879eb88cd5e14cf9ba3c2434aeaacc6271a35475
valvegan <valentinavaccari@hotmail.com>
2022-02-18 01:16:56 +0000

    registration

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import axios from 'axios';
 
-
+import {LoginView} from '../login-view/login-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
 export class MainView extends React.Component {
     constructor(){
         super();
+        //initial state is set to null
         this.state = {
           movies: [],
           selectedMovie: null
@@ -25,27 +26,43 @@ export class MainView extends React.Component {
           console.log(error);
         })
       }
-
+/*When a movie is clicked, this function is invoked and updates the state of the `selectedMovie` *property to that movie*/
       setSelectedMovie (newSelectedMovie){
           this.setState({
               selectedMovie: newSelectedMovie
           })
       }
+
+      /* When a user successfully logs in, this function updates the `user` property in state to that *particular user*/
+      onLoggedIn(user){
+        this.setState({
+          user
+        })
+      }
     
       render() {
-        const { movies, selectedMovie } = this.state;
+        const { movies, selectedMovie, user } = this.state;
 
+/* If there is no user, the LoginView is rendered. If there is a user logged in, the user details are *passed as a prop to the LoginView*/
+if (!user) return <LoginView onLoggedIn={
+  user=>this.onLoggedIn(user)
+}/>
+
+//before the movies have loaded
         if (movies.length === 0)
-          return <div className="main-view">The list is empty!</div>;
+          return <div className="main-view"/>;
       
           return (
             <div className="main-view">
-                {selectedMovie 
+                {
+                /*If the state of `selectedMovie` is not null, that selected movie will be returned. otherwise, all movies will be returned*/
+
+                selectedMovie 
                 ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie=> 
                     {this.setSelectedMovie(newSelectedMovie)}}/>
               : movies.map(movie => (
               <MovieCard key={movie._id} movie={movie}
-              onMovieClick={(movie)=> {this.setSelectedMovie(movie)}} 
+              onMovieClick={(newSelectedMovie)=> {this.setSelectedMovie(newSelectedMovie)}} 
               />))}
             </div>
           );
--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
 export class MovieCard extends React.Component {
   render() {
       const { movie, onMovieClick } = this.props;
-    return <div className="movie-card" onClick={()=>
-        {onMovieClick(movie)}}>{movie.Title}</div>;
+
+    return (
+    <div className="movie-card" onClick={()=>
+        {onMovieClick(movie)}}>{movie.Title}</div>)
   }
+}
+
+MovieCard.propTypes={
+  movie: PropTypes.shape({
+    Title: PropTypes.string.isRequired,
+    Genre: PropTypes.string.isRequired,
+    Director: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
+    Actors: PropTypes.array.isRequired,
+    ImagePath: PropTypes.string.isRequired,
+    Featured: boolean.isRequired
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired
 }
